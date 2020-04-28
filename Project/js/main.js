f9006e360ec94b3ef05af7143ac2ae3b3dd35d41
Tomer Shenkar <tomer.shenkar@gmail.com>
2020-04-28 13:14:42 +0300

    Add files via upload

--- Project/js/main.js
+++ Project/js/main.js
@@ -3,6 +3,7 @@ console.log("This is working");
 document.getElementById("unitMenu").addEventListener("click",openUnitMenu);
 document.getElementById("transMenu").addEventListener("click",openTransMenu);
 document.getElementById("urgMenu").addEventListener("click",openUrgMenu);
+document.getElementById("calcButton").addEventListener("click", calculate);
 
 function openUnitMenu(){
 	document.getElementById("ddUnitMenu").classList.toggle("active");
@@ -14,4 +15,10 @@ function openTransMenu(){
 
 function openUrgMenu(){
 	document.getElementById("ddUrgMenu").classList.toggle("active");
+}
+
+function calculate(){
+		var number = document.getElementById("textareaInput").value;
+		console.log(number);
+		document.getElementById("textareaOutput").value = number;
 }
