97559c12c346008fa7f82b2d17704ee2033a1b7e
Tomer Shenkar <tomer.shenkar@gmail.com>
2020-04-29 18:35:29 +0300

    Add files via upload

--- Project/js/main.js
+++ Project/js/main.js
@@ -1,24 +1,114 @@
 console.log("This is working");
 
-document.getElementById("unitMenu").addEventListener("click",openUnitMenu);
-document.getElementById("transMenu").addEventListener("click",openTransMenu);
-document.getElementById("urgMenu").addEventListener("click",openUrgMenu);
-document.getElementById("calcButton").addEventListener("click", calculate);
+document.getElementById("buttonCalc").addEventListener("click", calcuFun);
+document.getElementById("buttonPlus").addEventListener("click", addLine);
+	
+function calcuFun(){
 
-function openUnitMenu(){
-	document.getElementById("ddUnitMenu").classList.toggle("active");
+	var unitselection = document.getElementById("chosenunit").value;
+	var transselection = document.getElementById("chosentrans").value;
+	var urgselection = document.getElementById("chosenurg").value;
+	var taInput = document.getElementById("taIn").value;
+	
+	if(unitselection==="Units"){
+		if(transselection==="Associate"){
+			if(urgselection==="Non-Urgant"){
+				document.getElementById("taOut").value = taInput*132;
+			}
+			if(urgselection==="Semi-Urgant"){
+				document.getElementById("taOut").value = taInput*154;
+
+			}
+			if(urgselection==="Urgant"){
+				document.getElementById("taOut").value = taInput*179;
+
+			}
+			if(urgselection==="Panic"){
+				document.getElementById("taOut").value = taInput*220;
+			}
+		}
+		if(transselection==="Senior"){
+			if(urgselection==="Non-Urgant"){
+				document.getElementById("taOut").value = taInput*154;
+			}
+			if(urgselection==="Semi-Urgant"){
+				document.getElementById("taOut").value = taInput*176;
+			}
+			if(urgselection==="Urgant"){
+				document.getElementById("taOut").value = taInput*198;
+			}
+			if(urgselection==="Panic"){
+				document.getElementById("taOut").value = taInput*248;
+			}
+		}
+		else{
+			if(urgselection==="Non-Urgant"){
+				document.getElementById("taOut").value = taInput*176;
+			}
+			if(urgselection==="Semi-Urgant"){
+				document.getElementById("taOut").value = taInput*19;
+			}
+			if(urgselection==="Urgant"){
+				document.getElementById("taOut").value = taInput*220;
+			}
+			if(urgselection==="Panic"){
+				document.getElementById("taOut").value = taInput*275;
+			}
+		}
+	}
+	
+	else{
+		if(transselection==="Associate"){
+			if(urgselection==="Non-Urgant"){
+				document.getElementById("taOut").value = taInput*308;
+			}
+			if(urgselection==="Semi-Urgant"){
+				document.getElementById("taOut").value = taInput*352;
+			}
+			if(urgselection==="Urgant"){
+				document.getElementById("taOut").value = taInput*396;
+			}
+			if(urgselection==="Panic"){
+				document.getElementById("taOut").value = taInput*495;
+			}
+		}
+		
+		if(transselection==="Senior"){
+			if(urgselection==="Non-Urgant"){
+				document.getElementById("taOut").value = taInput*330;
+			}
+			if(urgselection==="Semi-Urgant"){
+				document.getElementById("taOut").value = taInput*374;
+			}
+			if(urgselection==="Urgant"){
+				document.getElementById("taOut").value = taInput*418;
+			}
+			if(urgselection==="Panic"){
+				document.getElementById("taOut").value = taInput*523
+			}
+		}
+		
+		else{
+			if(urgselection==="Non-Urgant"){
+				document.getElementById("taOut").value = taInput*352;
+			}
+			if(urgselection==="Semi-Urgant"){
+				document.getElementById("taOut").value = taInput*396;
+			}
+			if(urgselection==="Urgant"){
+				document.getElementById("taOut").value = taInput*440;
+			}
+			if(urgselection==="Panic"){
+				document.getElementById("taOut").value = taInput*550;
+			}
+		}
+	}	
 }
 
-function openTransMenu(){
-	document.getElementById("ddTransMenu").classList.toggle("active");
-}
-
-function openUrgMenu(){
-	document.getElementById("ddUrgMenu").classList.toggle("active");
-}
-
-function calculate(){
-		var number = document.getElementById("textareaInput").value;
-		console.log(number);
-		document.getElementById("textareaOutput").value = number;
+function addLine(){
+	document.getElementById("buttonCalc").disabled = true;
+	document.getElementById("chosenunit").disabled = true;
+	document.getElementById("chosentrans").disabled = true;
+	document.getElementById("chosenurg").disabled = true;
+	document.getElementById("taIn").disabled = true;
 }
